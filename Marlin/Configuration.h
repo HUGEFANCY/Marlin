411293e8bf5247c68a318993410ccfd17afd2acb
Chris <mail@chriswalter.org>
2020-08-21 12:29:14 +0200

    HDPE

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -741,7 +741,7 @@
 
 #define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
-  #define MAX_FEEDRATE_EDIT_VALUES    { 200, 200, 15, 16 } // ...or, set your own edit limits
+  #define MAX_FEEDRATE_EDIT_VALUES    { 200, 200, 20, 16 } // ...or, set your own edit limits
 #endif
 
 /**
@@ -754,7 +754,7 @@
 
 #define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
-  #define MAX_ACCEL_EDIT_VALUES       { 50, 50, 20, 10 } // ...or, set your own edit limits
+  #define MAX_ACCEL_EDIT_VALUES       { 80, 80, 15, 10 } // ...or, set your own edit limits
 #endif
 
 /**
